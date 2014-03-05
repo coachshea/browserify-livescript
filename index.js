d5bcd374b6928939cb5a9f3db975abd68ad59c4a
John Shea <coachshea@aol.com>
2014-03-05 16:25:11 -0500

    initial committ

---
+++ index.js
@@ -0,0 +1,31 @@
+var ls = require('LiveScript');
+var through = require('through');
+
+function compile(file, data) {
+    var compiled = ls.compile(data, { bare: true });
+
+    return compiled + '\n';
+}
+
+function isLive (file) {
+    return /.ls$/.test(file);
+}
+
+module.exports = function (file) {
+    if (!isLive(file)) return through();
+
+    var data = '';
+    return through(write, end);
+
+    function write (buf) { data += buf }
+    function end () {
+        var src;
+        try {
+            src = compile(file, data);
+        } catch (error) {
+            this.emit('error', error);
+        }
+        this.queue(src);
+        this.queue(null);
+    }
+};
